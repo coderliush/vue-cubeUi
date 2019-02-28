9d761f5a03b86e3d30ef56614c8ec0da85b9a145
unknown <liushuhao@sendcloud.im>
2019-02-28 19:04:39 +0800

    api

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import './cube-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// import Cube from 'cube-ui'
 import 'common/style/base.less'
 import 'amfe-flexible'
 import http from './services/http.js'
-
-// Vue.use(Cube)
 Vue.use(http)
 Vue.config.productionTip = false
 
